0fa2a7634227ce6014b70e5e79782ab49038c183
adevreactjs <adevreactjs@gmail.com>
2023-08-28 15:10:46 +0300

    settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+import { PostProps } from "@/types";
+
+const posts:PostProps[] = [
+ { title: 'React Testing',
+  excerpt:  'Learn React Testing'}
+]
 
 export default function Home() {
   return (
-    <main>
-      <div>hello</div>
+    <main className='container mx-auto px-10 mb-8'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div>
+          {posts.map(post => (
+            <div key={post.title} className='col-span-1 lg:col-span-12'>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className='lg:col-span-4 col-span-1'></div>
+      </div>
     </main>
   );
 }
