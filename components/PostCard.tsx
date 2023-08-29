178ea157ec01fbb36b0d42cbc3ef34debf79c692
adevreactjs <adevreactjs@gmail.com>
2023-08-29 13:26:08 +0300

    fix

---
+++ components/PostCard.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { PostProps } from '@/types';
+import React, { FC } from 'react';
+
+interface PostCardProps {
+    post: PostProps;
+}
+
+const PostCard:FC<PostCardProps> = ({post}) => {
+  return <div>{post.title} {post.excerpt}</div>;
+};
+
+export default PostCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
-import { PostProps } from "@/types";
+import { PostProps } from '@/types';
+import PostCard from '../../components/PostCard';
+import PostWidget from '../../components/PostWidget';
+import Categories from '../../components/Categories';
 
-const posts:PostProps[] = [
- { title: 'React Testing',
-  excerpt:  'Learn React Testing'}
-]
+const posts: PostProps[] = [{ title: 'React Testing', excerpt: 'Learn React Testing' }];
 
 export default function Home() {
   return (
     <main className='container mx-auto px-10 mb-8'>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div>
+        <div className='lg:col-span-8 col-span-1'>
           {posts.map(post => (
-            <div key={post.title} className='col-span-1 lg:col-span-12'>
-              {post.title}
-              {post.excerpt}
-            </div>
+            <PostCard key={post.title} post={post}/>
           ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'></div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget/>
+            <Categories/>
+          </div>
+        </div>
       </div>
     </main>
   );
