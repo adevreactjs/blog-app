d586e3f65a42179de58f02e4d5d2d61a32a5cffd
adevreactjs <adevreactjs@gmail.com>
2023-08-29 22:10:33 +0300

    add services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 import { PostProps } from '@/types';
 import PostCard from '../../components/PostCard';
+import PostWidget from '../../components/PostWidget';
+import Categories from '../../components/Categories';
+import FeaturedPosts from '../../components/FeaturedPosts';
 
 const posts: PostProps[] = [
   { title: 'React Testing1', excerpt: 'Learn React Testing' },
@@ -12,13 +15,19 @@ const posts: PostProps[] = [
 export default function Home() {
   return (
     <main className='container mx-auto px-10 mb-8'>
+      <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div>
+        <div className='lg:col-span-8 col-span-1'>
           {posts.map(post => (
             <PostCard key={post.title} post={post} />
           ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'></div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
     </main>
   );
