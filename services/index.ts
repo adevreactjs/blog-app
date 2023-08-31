7606488216e1a9e1eea7f9adbfc7588c290c8bbd
adevreactjs <adevreactjs@gmail.com>
2023-08-31 16:45:49 +0300

    add CMS

--- services/index.ts
+++ services/index.ts
@@ -1,7 +1,35 @@
 import { request, gql } from 'graphql-request';
 
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string;
+
 export const getPosts = async () => {
-    const query = gql`
+  const query = gql`
     query MyQuery {
-    }`
-}
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            category {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,42 @@ import PostCard from '../../components/PostCard';
 import PostWidget from '../../components/PostWidget';
 import Categories from '../../components/Categories';
 import FeaturedPosts from '../../components/FeaturedPosts';
+import { getPosts } from '../../services';
+import type { InferGetStaticPropsType, GetStaticProps } from 'next';
+import { useEffect } from 'react';
 
-const posts: PostProps[] = [
-  { title: 'React Testing1', excerpt: 'Learn React Testing' },
-  { title: 'React Testing2', excerpt: 'Learn React Testing' },
-  { title: 'React Testing3', excerpt: 'Learn React Testing' },
-];
+// const posts: PostProps[] = [
+//   { title: 'React Testing1', excerpt: 'Learn React Testing' },
+//   { title: 'React Testing2', excerpt: 'Learn React Testing' },
+//   { title: 'React Testing3', excerpt: 'Learn React Testing' },
+// ];
+// async function getData() {
+//   const res = (await getPosts()) || [];
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data');
+//   }
+
+//   return res.json();
+// }
+
+async function getData() {
+  const posts = (await getPosts()) || [];
+return posts
+}
 
 export default function Home() {
+  console.log(getData());
+
   return (
     <main className='container mx-auto px-10 mb-8'>
       <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map(post => (
+          {/* posts &&  {
+          posts.map(post => (
             <PostCard key={post.title} post={post} />
-          ))}
+          ))} */}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -32,3 +52,12 @@ export default function Home() {
     </main>
   );
 }
+
+// export async function getStaticProps() {
+//   const posts = (await getPosts()) || [];
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// }
